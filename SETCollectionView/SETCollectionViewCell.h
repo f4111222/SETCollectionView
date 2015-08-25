e4107f6cc3b7efc1e5ea37ef39ad574e2f75b0b1
蔡繼東 <Jacky@61-226-98-122.dynamic.hinet.net>
2015-08-25 11:30:38 +0800

    version1.0

---
+++ SETCollectionView/SETCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  SETCollectionViewCell.h
+//  SETCollectionView
+//
+//  Created by 蔡繼東 on 2015/8/25.
+//  Copyright (c) 2015年 蔡繼東. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SETCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ SETCollectionView/SETCollectionViewCell.m
@@ -0,0 +1,17 @@
+//
+//  SETCollectionViewCell.m
+//  SETCollectionView
+//
+//  Created by 蔡繼東 on 2015/8/25.
+//  Copyright (c) 2015年 蔡繼東. All rights reserved.
+//
+
+#import "SETCollectionViewCell.h"
+
+@implementation SETCollectionViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+@end
